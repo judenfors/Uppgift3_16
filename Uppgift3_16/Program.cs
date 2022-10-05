0d21265fe185752fc4910ae5bee2e6c0a7fea4b6
Admin <Admin@TEB123022>
2022-10-05 12:11:53 +0200

    Add project files.

---
+++ Uppgift3_16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift3_16
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur lång är den låten du vill spela");
+
+            int svar = int.Parse(Console.ReadLine());
+            
+            if(svar >= 245 && svar <= 420)
+            {
+                Console.WriteLine("Du får spela den här låten");
+
+            }
+            else
+            {
+                Console.WriteLine("Du får inte spela låten");
+            }
+        }
+    }
+}
